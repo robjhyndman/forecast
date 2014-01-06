b50dc76a9978e1d6147e3991b9ad7b1fd08e3de4
Earo Wang <earo.wang@gmail.com>
2014-01-06 17:41:26 +1100

    allow fractional results easter

--- R/calendar.R
+++ R/calendar.R
@@ -53,38 +53,58 @@ easter <- function(x, easter.mon = FALSE) {
   # Args:
   #   x: monthly, quarterly or weekly data
   #   easter.mon: An option including easter.mon
-  #
-  # ToDo:
-  #   Allow fractional results, if Easter spans Mar & Apr
   if (is.null(tsp(x))) {
     stop("We cannot handle a time series without time attributes.")
   }
   freq <- frequency(x)
+  date <- zoo::as.Date(time(x))
   start.yr <- start(x)[1L]
   end.yr <- end(x)[1L]
   yr.span <- seq(start.yr, end.yr)
+  gd.fri0 <- Easter(yr.span, -2L)
+  if (easter.mon) {
+    easter0 <- Easter(yr.span, 1L)
+  } else {
+    easter0 <- Easter(yr.span)
+  }
   if (freq == 12L) {
     fmat <- "%Y-%m"
-    yr.mon <- format(zoo::as.Date(time(x)), format = fmat)
-    gd.fri <- format(Easter(yr.span, shift = -2L), format = fmat)  # good fri
-    if (easter.mon) {
-      easter <- format(Easter(yr.span, shift = 1L), format = fmat) # easter mon
-    } else {
-      easter <- format(Easter(yr.span), format = fmat)
-    }
+    yr.mon <- format(date, format = fmat)
+    gd.fri <- format(gd.fri0, format = fmat)  # good fri
+    easter <- format(easter0, format = fmat) # easter mon
   } else if (freq == 4L) {
     fmat <- "%Y-%q"
-    yr.mon <- format(as.yearqtr(zoo::as.Date(time(x))), format = fmat) # yr.qtr
-    gd.fri <- format(as.yearqtr(Easter(yr.span, shift = -2L)), foramt = fmat)
-    if (easter.mon) {
-      easter <- format(as.yearqtr(Easter(yr.span, shift = 1L)), format = fmat)
-    } else {
-      easter <- format(as.yearqtr(Easter(yr.span)), format = fmat)
-    }
+    yr.mon <- format(as.yearqtr(date), format = fmat) # yr.qtr
+    gd.fri <- format(as.yearqtr(gd.fri0), format = fmat)
+    easter <- format(as.yearqtr(easter0), format = fmat)
   }
   span <- cbind(gd.fri, easter)  # the span of easter holidays
   hdays <- unlist(apply(span, 1, unique))
   dummies <- ifelse(yr.mon %in% hdays, 1L, 0L)
+  # Allow fractional results
+  denominator <- easter0 - gd.fri0 + 1L
+  last.mar <- as.timeDate(paste0(yr.span, "-03-31"))
+  dif <- difftimeDate(last.mar, gd.fri0, units = "days") + 1L
+  # Remove easter out of date range
+  if (date[1L] > as.character(last.mar[1L])) {
+    dif <- dif[-1L]
+  }
+  if (date[length(yr.mon)] < as.character(last.mar[length(last.mar)])) {
+    dif <- dif[-length(dif)]
+  }
+  replace <- dif > denominator[1L] | dif <= 0L
+  dif[replace] <- denominator[1L]  # Easter in the same month
+  # Start to insert the remaining part falling in Apr
+  index <- which(dif != denominator[1L])
+  if (length(index) != 0L) {
+    values <- denominator[1L] - dif[index]
+    new.index <- index[1L]
+    for (i in 1L:length(index)) {
+      dif <- append(dif, values = values[i], new.index)
+      new.index <- index[i + 1L] + i
+    }
+    dummies[dummies == 1L] <- dif/unclass(denominator[1L])
+  }
   out <- ts(dummies, start = tsp(x)[1L], frequency = freq)
   return(out)
 }
