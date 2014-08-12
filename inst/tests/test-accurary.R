d5dd46d5e174d7329b590efc51ae9adeba30b858
robjhyndman <robjhyndman@gmail.com>
2014-08-12 10:48:18 +1000

    Fixed some accuracy unit tests

--- inst/tests/test-accurary.R
+++ inst/tests/test-accurary.R
@@ -13,7 +13,8 @@ if(require(fpp) & require(testthat))
   	fcasts <- forecast(train, h = 6)
   	expect_that(dim(accuracy(fcasts)), equals(c(1, 7)))
   	expect_that(dim(accuracy(fcasts, test)), equals(c(2, 8)))
-  	expect_that(all(accuracy(fcasts, test, test = 1:2) == accuracy(fcasts, test)),
+  	expect_that(all(dim(accuracy(fcasts, test, test = 1:2)) == dim(accuracy(fcasts, test))),
               is_false())
+    expect_that(accuracy(fcasts, test = 1:198), equals(accuracy(fcasts)))
 	})
 }
