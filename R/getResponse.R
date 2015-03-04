188086fa8f7849402c7ca69a0f870fa722dc6d2a
Rob J Hyndman <robjhyndman@gmail.com>
2015-03-04 13:02:26 +1100

    Fixed arima.errors() to handle Box-Cox transformed models

--- R/getResponse.R
+++ R/getResponse.R
@@ -1,3 +1,8 @@
+# Functions to return the response variable for different models.
+# If a Box-Cox transformation is used, the series returned here should
+# be on the original scale, not the Box-Cox transformed scale.
+
+
 getResponse <- function(object,...) UseMethod("getResponse")
 
 getResponse.default <- function(object,...){
@@ -15,22 +20,22 @@ getResponse.lm <- function(object,...) {
 
 getResponse.Arima <- function(object,...) {
 	if (is.element("x", names(object))) 
-        x <- object$x
-    else
-    {
-    	series.name <- object$series
-    	if(is.null(series.name))
-    		stop("missing component series in Arima model")
-    	else
-    	{
-	    	x <- try(eval.parent(parse(text = series.name)),silent=TRUE)
-	    	if(is.element("try-error",class(x))) # Try one level further up the chain
-	    		x <- try(eval.parent(parse(text = series.name),2),silent=TRUE)
-        if(is.element("try-error",class(x))) # Give up
-          return(NULL)
-    	}
-	}
-	return(as.ts(x))
+    x <- object$x
+  else
+  {
+  	series.name <- object$series
+  	if(is.null(series.name))
+  		stop("missing component series in Arima model")
+  	else
+  	{
+    	x <- try(eval.parent(parse(text = series.name)),silent=TRUE)
+    	if(is.element("try-error",class(x))) # Try one level further up the chain
+    		x <- try(eval.parent(parse(text = series.name),2),silent=TRUE)
+      if(is.element("try-error",class(x))) # Give up
+        return(NULL)
+  	}
+  }
+  return(as.ts(x))
 }
 
 getResponse.fracdiff <- function(object, ...) {
