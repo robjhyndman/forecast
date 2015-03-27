490318bcfaf7e715bab9bd990f2f140a871acca6
Rob J Hyndman <robjhyndman@gmail.com>
2015-03-27 15:01:33 +1100

    Added class check to accuracy()

--- R/errors.R
+++ R/errors.R
@@ -173,6 +173,8 @@ trainingaccuracy <- function(f,test,d, D)
 
 accuracy <- function(f,x,test=NULL,d=NULL,D=NULL)
 {
+  if(!any(is.element(class(f), c("mforecast","forecast","ts","integer","numeric")))
+    stop("First argument should be a forecast object or a time series.")
   if(is.element("mforecast", class(f)))
     return(accuracy.mforecast(f,x,test,d,D))
 
