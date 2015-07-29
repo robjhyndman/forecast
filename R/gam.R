40b46e5d71d4e543271cc411e53a996594d1ec37
terrytangyuan <terrytangyuan@gmail.com>
2015-07-29 09:14:33 -0400

    added gam.R

--- R/gam.R
+++ R/gam.R
@@ -1,2 +1,12 @@
+tsgam <- function(formula,data,lambda=NULL,...)
+{
+  return(tswp(formula,data,lambda=NULL,model="gam",...))
+}
+
+forecast.gam <- function(object, newdata, h=10, level=c(80,95), fan=FALSE, lambda=object$lambda, ts=TRUE, ...)
+{
+  return(forecast.wp(object, newdata, h=10, level=c(80,95), fan=FALSE, lambda=object$lambda, ts=TRUE, ...))
+}
+
 
 
