f91267e2264d4ef9523486bde98092ba4c8d3f2a
terrytangyuan <terrytangyuan@gmail.com>
2015-08-12 10:04:12 -0400

    more tests for bats class methods

--- inst/tests/test-bats.R
+++ inst/tests/test-bats.R
@@ -11,9 +11,14 @@ if(require(testthat))
 	})
 	
 	test_that("functionalities and exceptions of bats", {
+	  abc <- rnorm(50, 5, 1)
+	  fit <- bats(abc, use.box.cox = TRUE, use.parallel = FALSE)
+	  expect_that(print(fit), not(throws_error()))
+	  expect_that(residuals(fit), not(throws_error()))
+	  expect_that(plot(fit), not(throws_error()))
 	  expect_that(bats(abc, use.box.cox = TRUE, use.parallel = TRUE), not(throws_error()))
 	  expect_that(bats(1, use.box.cox = TRUE, use.parallel = FALSE), not(throws_error()))
-	  
+	  expect_that(bats(-1, use.box.cox = TRUE, use.parallel = FALSE), not(throws_error()))
 	})
 	
 }
