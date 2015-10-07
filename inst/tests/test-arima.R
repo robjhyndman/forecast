e77d72a20da633433caf4dd6a64d07a2f8615205
David Shaub <dashaub@users.noreply.github.com>
2015-10-07 09:50:10 -0600

    Add more unit tests
    forecast.ar(), as.character.Arima(), and refit of Arima models

--- inst/tests/test-arima.R
+++ inst/tests/test-arima.R
@@ -18,4 +18,31 @@ if(require(fpp) & require(testthat))
   	fit <- Arima(a10, order = c(1, 1, 1), seasonal = c(0, 1, 1))
   	expect_that(arima.errors(fit), equals(a10))
 	})
+	
+	test_that("tests for forecast.ar()", {
+	fitar <- ar(taylor)
+	arfc <- forecast.ar(fitar)$mean
+	expect_true(all(arfc == forecast.ar(fitar, bootstrap = TRUE, npaths = 100)$mean))
+	expect_true(all(arfc == forecast.ar(fitar, fan = TRUE)$mean))
+	expect_error(forecast.ar(fitar, level = -10))
+	expect_error(forecast.ar(fitar, level = 110))
+	expect_true(all(arfc + 1 == forecast.ar(fitar, lambda = 1)$mean))
+	})
+	
+	test_that("tests for as.character.Arima()", {
+  	expect_output(as.character(auto.arima(woolyrnq, test = "adf")), regexp = "ARIMA")
+	})
+	
+	test_that("tests for refit Arima", {
+	# Refit with drift
+	fitarima <- auto.arima(austa, test = "pp")
+	newdata <- forecast(austa)$mean
+	refitarima <- Arima(c(austa, newdata), model = fitarima)
+	expect_true(all(arimaorder(fitarima) == arimaorder(refitarima)))
+	# Refit seasonal model
+	fitarima <- auto.arima(cafe)
+	newdata <- forecast(cafe)$mean
+	refitarima <- Arima(c(cafe, newdata), model = fitarima)
+	expect_true(all(arimaorder(fitarima) == arimaorder(refitarima)))
+	})
 }
