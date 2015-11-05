b8c4edda5d4d924b6d00dd4e93b758eddc67affb
Rob J Hyndman <robjhyndman@gmail.com>
2015-11-05 11:56:32 +1100

    subset.ts rewritten to have greater functionality and to catch many errors

--- R/subset.R
+++ R/subset.R
@@ -1,18 +1,45 @@
 subset.ts <- function(x, subset=NULL, month=NULL, quarter=NULL, season=NULL, ...)
 {
   if(!is.null(subset))
+  {
+    if(length(subset) != length(x))
+      stop("subset must be the same length as x")
     return(subset.default(x,subset))
+  }
   else if(frequency(x) <= 1)
     stop("Data must be seasonal")
   if(!is.null(month))
-    season <- pmatch(month,month.name,duplicates.ok=TRUE)
+  {
+    if(frequency(x) != 12)
+      stop("Data is not monthly")
+    if(is.character(month))
+      season <- pmatch(tolower(month),tolower(month.name),duplicates.ok=TRUE)
+    else
+      season <- month
+    season <- na.omit(season)
+    if(length(season) == 0L)
+      stop("No recognizable months")
+    if(min(season) < 1L | max(season) > 12L)
+      stop("Months must be between 1 and 12")
+  }
   else if(!is.null(quarter))
-    season <- quarter
+  {
+    if(frequency(x) != 4)
+      stop("Data is not quarterly")
+    if(is.character(quarter))
+      season <- pmatch(tolower(quarter),paste("q",1:4,sep=""),duplicates.ok=TRUE)
+    else
+      season <- quarter
+    season <- na.omit(season)
+    if(length(season) == 0L)
+      stop("No recognizable quarters")
+    if(min(season) < 1 | max(season) > 4)
+      stop("Quarters must be between 1 and 4")
+  }
   else if(is.null(season))
     stop("No subset specified")
-  x.time <- subset(time(x),cycle(x)==season)
-  if(length(season)==1) # one value per year
-    return(ts(subset(x,cycle(x)==season),frequency=1,start=x.time[1]))
-  else # ignore ts attributes
-    return(subset(x,cycle(x)==season))
+
+  start <- head(time(x)[is.element(cycle(x), season)],1)
+  x <- subset.default(x, is.element(cycle(x), season))
+    return(ts(x, frequency=length(season), start=start))
 }
