f6a9fd0257c21392193fbb1c74f5eabedc758a4d
Rob J Hyndman <robjhyndman@gmail.com>
2016-04-23 17:33:52 +1000

    Fixed bug in msts.

--- R/msts.R
+++ R/msts.R
@@ -1,14 +1,12 @@
 msts <- function(data, seasonal.periods, ts.frequency=floor(max(seasonal.periods)), ...)
 {
-	#if(is.element("ts",class(data)))
-	#{
-		#if(frequency(data) > 1L)
-		#	object <- data
-		#else
-	#		object <- ts(data=data, frequency=ts.frequency, ...)
-	#}
-  #else
-		object <- ts(data=data, frequency=ts.frequency, ...)
+  if(ts.frequency <= 1L)
+    stop("frequency must be greater than 1 for an msts object")
+
+	if(is.element("ts",class(data)) & frequency(data) == ts.frequency)
+		object <- data
+  else
+	  object <- ts(data=data, frequency=ts.frequency, ...)
 	if(length(seasonal.periods) > 1L)
 	{
 		class(object) <- c("msts", "ts")
