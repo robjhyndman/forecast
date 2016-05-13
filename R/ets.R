6a1efb947c5682ed95a81029d996e3cbeb1b2fc7
mitchelloharawild <mitch.ohara-wild@monash.edu>
2016-05-13 17:32:18 +1000

    Removed hfitted from namespace and added documentation

--- R/ets.R
+++ R/ets.R
@@ -1016,7 +1016,7 @@ coef.ets <- function(object,...)
 
 fitted.ets <- function(object, h=1, ...){
   if(h==1){
-    return(stats:::fitted.default(object))
+    return(object$fitted)
   }
   else{
     return(hfitted(object=object, h=h, FUN=ets, ...))
