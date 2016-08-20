db3f5091adb2c5e1f67a47fd7a4e5fa818f566cf
robjhyndman <robjhyndman@gmail.com>
2016-08-20 16:25:26 +1000

    Reverted DM change

--- R/DM2.R
+++ R/DM2.R
@@ -19,8 +19,8 @@ dm.test <- function (e1, e2, alternative = c("two.sided", "less", "greater"), h 
   }
   
   n <- length(d)
-  k <- ((n + 1 - 2 * h + (h/n) * (h-1))/n)^(1/2)
-  STATISTIC <- STATISTIC / k
+  k <- ((n + 1 - 2*h + (h/n) * (h-1))/n)^(1/2)
+  STATISTIC <- STATISTIC * k
   names(STATISTIC) <- "DM"
   if (alternative == "two.sided")
     PVAL <- 2 * pt(-abs(STATISTIC), df = n-1)
