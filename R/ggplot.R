11830caf108fa2ba1a73eb919466283edcf0afc5
mitchelloharawild <mitch.ohara-wild@monash.edu>
2017-01-04 17:07:34 +1100

    Removed need for importing from methods
    (Since colorspace already depends on it)

--- R/ggplot.R
+++ R/ggplot.R
@@ -1324,13 +1324,13 @@ blendHex <- function(mixcol, seqcol, alpha=1){
   #transform to hue/lightness/saturation colorspace
   seqcol <- grDevices::col2rgb(seqcol, alpha = TRUE)
   mixcol <- grDevices::col2rgb(mixcol, alpha = TRUE)
-  seqcolHLS <- as(colorspace::RGB(R = seqcol[1,]/255, G = seqcol[2,]/255, B = seqcol[3,]/255), Class = "HLS")
-  mixcolHLS <- as(colorspace::RGB(R = mixcol[1,]/255, G = mixcol[2,]/255, B = mixcol[3,]/255), Class = "HLS")
+  seqcolHLS <- suppressWarnings(colorspace::coerce(colorspace::RGB(R = seqcol[1,]/255, G = seqcol[2,]/255, B = seqcol[3,]/255), structure(NULL, class="HLS")))
+  mixcolHLS <- suppressWarnings(colorspace::coerce(colorspace::RGB(R = mixcol[1,]/255, G = mixcol[2,]/255, B = mixcol[3,]/255), structure(NULL, class="HLS")))
   
   #copy luminence
   mixcolHLS@coords[, "L"] <- seqcolHLS@coords[, "L"]
   mixcolHLS@coords[, "S"] <- alpha*mixcolHLS@coords[, "S"] + (1-alpha)*seqcolHLS@coords[, "S"]
-  mixcolHex <- as(mixcolHLS, "RGB")
+  mixcolHex <- suppressWarnings(colorspace::coerce(mixcolHLS, structure(NULL, class="RGB")))
   mixcolHex <- colorspace::hex(mixcolHex)
   
   return(mixcolHex)
