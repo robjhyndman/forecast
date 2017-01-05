aa524bbc809619e5b0c9fe01c4b5c57905bdcce5
robjhyndman <robjhyndman@gmail.com>
2017-01-05 14:08:39 +1100

    arima.string now more informative when regression variables included.

--- R/newarima2.R
+++ R/newarima2.R
@@ -617,14 +617,22 @@ arima.string <- function(object, padding=FALSE)
   result <- paste("ARIMA(",order[1],",",order[2],",",order[3],")",sep="")
   if(order[7]>1 & sum(order[4:6]) > 0)
     result <- paste(result,"(",order[4],",",order[5],",",order[6],")[",order[7],"]",sep="")
-  if(is.element("constant",names(object$coef)) | is.element("intercept",names(object$coef)))
-    result <- paste(result,"with non-zero mean")
-  else if(is.element("drift",names(object$coef)))
-    result <- paste(result,"with drift        ")
-  else if(order[2]==0 & order[5]==0)
-    result <- paste(result,"with zero mean    ")
+  if(!is.null(object$xreg))
+  {
+    if(NCOL(object$xreg)==1 & is.element("drift",names(object$coef)))
+      result <- paste(result,"with drift        ")
+    else
+      result <- paste("Regression with",result,"errors")
+  }
   else
-    result <- paste(result,"                  ")
+  {
+    if(is.element("constant",names(object$coef)) | is.element("intercept",names(object$coef)))
+      result <- paste(result,"with non-zero mean")
+    else if(order[2]==0 & order[5]==0)
+      result <- paste(result,"with zero mean    ")
+    else
+      result <- paste(result,"                  ")
+  }
   if(!padding)
     #Strip trailing spaces
     result <- gsub("[ ]*$","",result)
