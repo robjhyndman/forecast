d9014746b3d0196eb56885a2d2d74c0f0321822c
robjhyndman <robjhyndman@gmail.com>
2017-02-02 09:27:00 +1100

    Removed unit test of autoplot.seas()
    Reason: Travis can't install seasonal package.

--- tests/testthat/test-ggplot.R
+++ tests/testthat/test-ggplot.R
@@ -6,7 +6,7 @@ if(require(testthat))
   test_that("tests for autoplot/gg functions", {
     library(ggplot2)
     lungDeaths <- cbind(mdeaths, fdeaths)
-    
+
     ggAcf(wineind)
     autoplot(Acf(wineind))
     identical(ggAcf(wineind, plot=FALSE)$acf, acf(wineind, plot=FALSE, lag.max = 24)$acf)
@@ -16,15 +16,15 @@ if(require(testthat))
     ggCcf(mdeaths, fdeaths)
     autoplot(Ccf(mdeaths, fdeaths))
     identical(ggCcf(mdeaths, fdeaths, plot=FALSE)$acf, ccf(mdeaths, fdeaths, plot=FALSE, type="correlation", lag.max = 24)$acf)
-    
+
     arimafit <- Arima(USAccDeaths, order = c(1,1,1), seasonal = c(1,1,1))
     autoplot(arimafit)
     autoplot(arimafit, type = "ma")
     autoplot(arimafit, type = "ar")
-    
+
     arfit <- ar(USAccDeaths)
     autoplot(arfit)
-    
+
     decomposefit <- decompose(USAccDeaths)
     autoplot(decomposefit)
     etsfit <- ets(USAccDeaths, model="ANA")
@@ -33,44 +33,44 @@ if(require(testthat))
     autoplot(structfit)
     stlfit <- stl(USAccDeaths, s.window = "periodic")
     autoplot(stlfit)
-    
-    seasfit <- seasonal::seas(USAccDeaths)
-    autoplot(seasfit)
-    
+
+    #seasfit <- seasonal::seas(USAccDeaths)
+    #autoplot(seasfit)
+
     etsfcast <- forecast(etsfit)
     autoplot(etsfcast)
     autoplot(etsfcast, PI = FALSE)
-    
+
     lmfit <- lm(mpg ~ disp, data=mtcars)
     lmfcast <- forecast(lmfit, newdata=data.frame(disp=214))
     autoplot(lmfcast)
-    
+
     mfcast <- forecast(lungDeaths)
     autoplot(mfcast)
-    
+
     ggtsdisplay(USAccDeaths, plot.type = "spectrum")
     ggtsdisplay(USAccDeaths, plot.type = "partial")
     ggtsdisplay(USAccDeaths, plot.type = "histogram")
     ggtsdisplay(USAccDeaths, plot.type = "scatter", theme=ggplot2::theme_bw())
-    
+
     gglagplot(woolyrnq, lags=2)
     gglagplot(lungDeaths, lags=2)
     gglagplot(WWWusage, do.lines = FALSE, colour = FALSE, labels = TRUE)
-    
+
     gglagchull(woolyrnq, lags=4)
-    
+
     ggmonthplot(woolyrnq)
-    
+
     ggseasonplot(woolyrnq, year.labels = TRUE, year.labels.left = TRUE)
     ggseasonplot(USAccDeaths, polar=TRUE, col=1:5, continuous = TRUE)
-    
+
     splinefit <- splinef(airmiles, h=5)
     autoplot(splinefit)
-    
+
     autoplot(USAccDeaths)
     autoplot(lungDeaths)
     autoplot(lungDeaths, facet=TRUE)
-    
+
     autoplot(USAccDeaths) + geom_forecast()
     autoplot(USAccDeaths) + geom_forecast(etsfcast, series="ETS")
     autoplot(lungDeaths) + geom_forecast()
@@ -78,7 +78,7 @@ if(require(testthat))
     autoplot(lungDeaths) + geom_forecast(mfcast)
     autoplot(lungDeaths) + geom_forecast(mfcast, series = TRUE)
     autoplot(lungDeaths, facet=TRUE) + geom_forecast()
-    
+
     gghistogram(USAccDeaths, add.kde = TRUE)
   })
 }
