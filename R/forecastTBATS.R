34610340af48723d0e1ff076631821e3d85429dc
mitchelloharawild <mitch.ohara-wild@monash.edu>
2017-02-21 19:02:28 +1100

    Added level information to interval column names
    Fixes #500

--- R/forecastTBATS.R
+++ R/forecastTBATS.R
@@ -123,6 +123,7 @@ forecast.tbats <- function(object, h, level=c(80,95), fan=FALSE, biasadj=NULL, .
 	y.forecast <- msts(y.forecast, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { ts.frequency}), ts.frequency=ts.frequency, start=fcast.start.time)
   upper.bounds <- msts(upper.bounds, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { ts.frequency}), ts.frequency=ts.frequency, start=fcast.start.time)
   lower.bounds <- msts(lower.bounds, seasonal.periods=(if(!is.null(object$seasonal.periods)) { object$seasonal.periods} else { ts.frequency}), ts.frequency=ts.frequency, start=fcast.start.time)
+  colnames(upper.bounds) <- colnames(lower.bounds) <- paste0(level, "%")
   
   forecast.object <- list(model=object, mean=y.forecast, level=level, x=x, series=object$series,
 	                        upper=upper.bounds, lower=lower.bounds, fitted=fitted.values,
