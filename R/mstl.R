8014d36ef27ce690851eea59bd60b0cac98d6212
Unknown <mitch.ohara-wild@monash.edu>
2018-04-23 10:40:55 +1000

    Fixed column name rounding for forecast.stlm
    Resolves #678

--- R/mstl.R
+++ R/mstl.R
@@ -480,7 +480,7 @@ forecast.stlm <- function(object, h = 2 * object$m, level = c(80, 95), fan = FAL
   {
     mp <- seasonal.periods[i]
     n <- NROW(object$stl)
-    colname <- paste0("Seasonal", mp)
+    colname <- paste0("Seasonal", round(mp, 2))
     seascomp[, i] <- rep(object$stl[n - rev(seq_len(mp)) + 1, colname], trunc(1 + (h - 1) / mp))[seq_len(h)]
   }
   lastseas <- rowSums(seascomp)
