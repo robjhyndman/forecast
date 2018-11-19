1afaae9bb122b5d58c3f632f6d275941c3fe4b76
Mitchell <mitch.ohara-wild@monash.edu>
2018-11-19 21:24:52 +1100

    Removed bootstrap and npaths from lagwalk docs (passed via ...)

--- R/naive.R
+++ R/naive.R
@@ -209,9 +209,6 @@ rwf <- function(y, h=10, drift=FALSE, level=c(80, 95), fan=FALSE, lambda=NULL, b
 #' @param level Confidence levels for prediction intervals.
 #' @param fan If TRUE, level is set to seq(51,99,by=3). This is suitable for
 #' fan plots.
-#' @param bootstrap If TRUE, use a bootstrap method to compute prediction intervals.
-#' Otherwise, assume a normal distribution.
-#' @param npaths Number of bootstrapped sample paths to use if \code{bootstrap==TRUE}.
 #' @param x Deprecated. Included for backwards compatibility.
 #' @inheritParams forecast
 #' 
