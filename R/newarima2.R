cac89616b79d1cf5d0a5ee1f41339976d91c9a5b
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-18 10:26:37 +1100

    Fixed issue where auto.arima pdqPDQ inputs are named

--- R/newarima2.R
+++ R/newarima2.R
@@ -851,7 +851,7 @@ newmodel <- function(p, d, q, P, D, Q, constant, results) {
   n <- nrow(results)
   for (i in 1:n)
   {
-    if (identical(c(p, d, q, P, D, Q, constant), results[i, 1:7])) {
+    if (all(c(p, d, q, P, D, Q, constant) == results[i, 1:7])) {
       return(FALSE)
     }
   }
