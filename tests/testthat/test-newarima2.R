809e1a55711537c6228f25ecaec815959b1eaf30
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-05 17:56:28 +1100

    Revert "Chanegd kpss default lag selection to match urca and tseries"
    This reverts commit 6c86c6390beb6f0007d13926ce486e7a63ebf9f4.
    
    Resolves #776

--- tests/testthat/test-newarima2.R
+++ tests/testthat/test-newarima2.R
@@ -23,7 +23,7 @@ if (require(testthat)) {
 
   test_that("test parallel = TRUE and stepwise = FALSE for auto.arima()", {
     skip_on_travis()
-    expect_equal(auto.arima(WWWusage, parallel = TRUE, stepwise = FALSE)$arma, c(4L, 0L, 0L, 0L, 1L, 0L, 0L))
+    expect_equal(auto.arima(WWWusage, parallel = TRUE, stepwise = FALSE)$arma, c(3L, 0L, 0L, 0L, 1L, 1L, 0L))
   })
 
   
