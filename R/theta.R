4ca7d83b418d1ab2698748d9a1052b726399e45a
Rob J Hyndman <robjhyndman@gmail.com>
2019-10-01 08:00:55 +1000

    Prevent thetaf error on short seasonal series

--- R/theta.R
+++ R/theta.R
@@ -79,7 +79,7 @@ thetaf <- function(y, h=ifelse(frequency(y) > 1, 2 * frequency(y), 10),
   n <- length(x)
   x <- as.ts(x)
   m <- frequency(x)
-  if (m > 1 && !is.constant(x) && n > m) {
+  if (m > 1 && !is.constant(x) && n > 2*m) {
     r <- as.numeric(acf(x, lag.max = m, plot = FALSE)$acf)[-1]
     stat <- sqrt((1 + 2 * sum(r[-m] ^ 2)) / n)
     seasonal <- (abs(r[m]) / stat > qnorm(0.95))
