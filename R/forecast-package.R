48b11dd8586045c90687ab7635e075bc226a26d5
robjhyndman <robjhyndman@gmail.com>
2019-11-23 13:30:00 +1100

    Add palette to imports

--- R/forecast-package.R
+++ R/forecast-package.R
@@ -7,7 +7,7 @@
 #' @importFrom zoo rollmean as.Date as.yearqtr
 #' @importFrom timeDate as.timeDate isBizday difftimeDate Easter as.Date.timeDate
 #' @importFrom nnet nnet
-#' @importFrom grDevices gray heat.colors nclass.FD
+#' @importFrom grDevices gray heat.colors nclass.FD palette
 #' @importFrom graphics abline axis grid layout lines mtext par plot points polygon text title hist
 #' @importFrom stats Box.test acf approx ar arima arima.sim as.ts complete.cases cycle decompose diffinv end extractAIC fitted formula frequency window filter na.contiguous spec.ar hatvalues is.ts ksmooth lm lsfit loess median model.frame na.exclude na.omit na.pass optim optimize pf plot.ts poly predict pt qnorm qt quantile residuals rnorm runif sd simulate smooth.spline start stl supsmu terms time ts tsp tsp<- tsdiag var logLik nobs napredict
 #' @importFrom stats aggregate as.formula is.mts reformulate
