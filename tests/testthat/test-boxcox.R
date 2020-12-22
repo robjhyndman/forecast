d6ebe534f802d9cbcae9d4eb0717fb83217d537d
Rob Hyndman <robjhyndman@gmail.com>
2020-12-22 11:52:33 +1100

    Added tolerance to BoxCox automated selection tests

--- tests/testthat/test-boxcox.R
+++ tests/testthat/test-boxcox.R
@@ -15,7 +15,7 @@ if (require(testthat)) {
     x <- fracdiff::fracdiff.sim(100, ma = -.4, d = .3)$series
     fit <- arfima(x)
     expect_true(all.equal(forecast(fit), forecast(fit, biasadj=TRUE)))
-    
+
     #arima
     fit1 <- Arima(USAccDeaths, order = c(0,1,1), seasonal = c(0,1,1), lambda = 0.5, biasadj = TRUE)
     fit2 <- auto.arima(USAccDeaths, max.p=0, max.d=1, max.q=1, max.P=0, max.D=1, max.Q=1, lambda = 0.5, biasadj = TRUE)
@@ -35,20 +35,20 @@ if (require(testthat)) {
     # fit <- tbats(USAccDeaths, use.box.cox = TRUE, biasadj = TRUE)
     # expect_true(all.equal(forecast(fit), forecast(fit, biasadj=TRUE)))
   })
-  
+
   test_that("tests for automatic lambda selection in BoxCox transformation", {
     lambda_auto <- BoxCox.lambda(USAccDeaths)
-    
+
     # lm
     fit <- tslm(USAccDeaths ~ trend, lambda = "auto", biasadj = TRUE)
-    expect_equal(as.numeric(fit$lambda), lambda_auto)
-    
+    expect_equal(as.numeric(fit$lambda), lambda_auto, tolerance=1e-5)
+
     # ets
     fit <- ets(USAccDeaths, model = "ANA", lambda = "auto", biasadj = TRUE)
-    expect_equal(as.numeric(fit$lambda), lambda_auto)
-    
+    expect_equal(as.numeric(fit$lambda), lambda_auto, tolerance=1e-5)
+
     # arima
     fit <- Arima(USAccDeaths, order = c(0,1,1), seasonal = c(0,1,1), lambda = "auto", biasadj = TRUE)
-    expect_equal(as.numeric(fit$lambda), lambda_auto)
+    expect_equal(as.numeric(fit$lambda), lambda_auto, tolerance=1e-5)
   })
 }
